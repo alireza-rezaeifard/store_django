ac1a5a323e731382bd00148c4617a41ee721b56f
alireza <alireza.rezaeifard13811@gmail.com>
2023-12-02 20:23:30 +0330

    update some bugs
    Co-Authored-By: AliReza Mousavi <83025404+SeyedAlireza79@users.noreply.github.com>

--- csv_retails/urls.py
+++ csv_retails/urls.py
@@ -1,9 +1,12 @@
 # myapp/urls.py
 from django.urls import path
 from .views import import_csv, add_data, search_data
+from . import views
+
 
 urlpatterns = [
     path('import-csv/', import_csv, name='import_csv'),
     path('add-data/', add_data, name='add_data'),
     path('search-data/', search_data, name='search_data'),
+    path('search-csv/', views.search_csv, name='search_csv'),
 ]
--- csv_retails/views.py
+++ csv_retails/views.py
@@ -3,9 +3,15 @@ import csv
 from .models import Fashion
 from .forms import FashionForm
 from django.db.models import Q
+from django.conf import settings
+import os
+
+
 
 def import_csv(request):
-    with open(r'store_django\csv_retails\retails.csv', 'r') as file:
+    csv_file_path = os.path.join(settings.BASE_DIR, 'csv_retails', 'retails.csv')
+    
+    with open(csv_file_path, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             Fashion.objects.create(
@@ -17,20 +23,61 @@ def import_csv(request):
             )
     return HttpResponse('Data imported successfully!')
 
+def search_csv(request):
+    query = request.GET.get('q', '')
+    results = []
+
+    csv_file_path = os.path.join(settings.BASE_DIR, 'csv_retails', 'retails.csv')
+
+    with open(csv_file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            # اگر query در هر یک از فیلدها وجود داشته باشد، این ردیف به نتایج اضافه می‌شود.
+            if any(query.lower() in str(value).lower() for value in row.values()):
+                results.append(row)
+
+    return render(request, 'search_csv.html', {'results': results, 'query': query})
+
 def add_data(request):
     if request.method == 'POST':
         form = FashionForm(request.POST)
         if form.is_valid():
             form.save()
+
+            # ذخیره‌ی داده‌های ورودی در فایل CSV
+            csv_file_path = os.path.join(settings.BASE_DIR, 'csv_retails', 'retails.csv')
+            with open(csv_file_path, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([
+                    form.cleaned_data['Customer_Reference_ID'],
+                    form.cleaned_data['Item_Purchased'],
+                    form.cleaned_data['Purchase_Amount'],
+                    form.cleaned_data['Review_Rating'],
+                    form.cleaned_data['Payment_Method'],
+                ])
+
             return redirect('add_data')
     else:
         form = FashionForm()
     return render(request, 'add_data.html', {'form': form})
 
 def search_data(request):
-    query = request.GET.get('q')
-    if query:
-        results = Fashion.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    customer_reference_id = request.GET.get('customer_reference_id')
+
+    csv_file_path = os.path.join(settings.BASE_DIR, 'csv_retails', 'retails.csv')
+
+    if customer_reference_id:
+        results = []
+        with open(csv_file_path, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row['Customer_Reference_ID'] == customer_reference_id:
+                    results.append(row)
     else:
-        results = Fashion.objects.all()
-    return render(request, 'search_data.html', {'results': results, 'query': query})
+        results = []
+        with open(csv_file_path, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                results.append(row)
+
+    return render(request, 'search_data.html', {'results': results, 'customer_reference_id': customer_reference_id})
